c83f2fde548cadd0474392aba3df29125378de89
fenghuang81 <stevenxianjuewu@gmail.com>
2025-02-07 16:33:55 -0500

    First commit

---
+++ 1 - Partie Orientée-objet (Java)/Movie.java
@@ -0,0 +1,34 @@
+// Project CSI2120/CSI2520
+// Winter 2025
+// Robert Laganiere, uottawa.ca
+
+public class Movie {
+	
+	private int movieID;
+	private String title;
+	
+	// constructs a movie
+    public Movie(int id, String title) {
+	
+		movieID= id;
+		this.title= title;		
+	}
+	
+	// gets the ID
+	public int getID() {
+	
+		return movieID;
+	}
+
+	// get the movie title
+	public String getTitle() {
+	
+		return title;
+	}
+	
+	// string representation
+	public String toString() {
+      
+       return "["+movieID+"]:"+title;	  
+	}
+}
