12d196c82ab41b6f5fcb93fcb90326a7233b92cc
fenghuang81 <stevenxianjuewu@gmail.com>
2025-02-09 21:35:13 -0500

    Add constructor for Recommendation class

--- 1 - Partie Orientée-objet (Java)/Recommendation.java
+++ 1 - Partie Orientée-objet (Java)/Recommendation.java
@@ -1,6 +1,13 @@
 public class Recommendation {
     private User user;
     private Movie movie;
-    private int score;
+    private float score;
     private int nUsers;
+
+    public Recommendation(User user, Movie movie, float score, int nUsers) {
+        this.user = user;
+        this.movie = movie;
+        this.score = score;
+        this.nUsers = nUsers;
+    }
 }
