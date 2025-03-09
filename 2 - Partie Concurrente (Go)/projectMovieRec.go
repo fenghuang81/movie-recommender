669f06f3accd8620bdb29f349ea3d43d15856b54
fenghuang81 <stevenxianjuewu@gmail.com>
2025-03-09 18:53:35 -0400

    Add Go starting files

---
+++ 2 - Partie Concurrente (Go)/projectMovieRec.go
@@ -0,0 +1,266 @@
+// Project CSI2120/CSI2520
+// Winter 2025
+// Robert Laganiere, uottawa.ca
+
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"sync"
+	"os"
+	"log"
+	"time"
+	"strconv"
+	"runtime"
+)
+
+// movies with rating greater or equal are considered 'liked'
+const iLiked float64 = 3.5
+
+// Define the Recommendation type
+type Recommendation struct {
+    userID int     // recommendation for this user
+ 	movieID int    // recommended movie ID
+ 	movieTitle string    // recommended movie title
+	score float32  // probability that the user will like this movie
+	nUsers int     // number of users who likes this movie
+}
+
+// get the probability that this user will like this movie
+func (r Recommendation) getProbLike() float32 {
+   return r.score/(float32)(r.nUsers)
+}
+
+// Define the User type
+// and its list of liked items
+type User struct {
+	userID int
+	liked []int     // list of movies with ratings >= iLiked
+	notLiked []int  // list of movies with ratings < iLiked
+}
+
+func (u User) getUser() int {
+   return u.userID
+}
+
+func (u *User) setUser(id int) {
+   u.userID= id
+}
+
+func (u *User) addLiked(id int) {
+   u.liked= append(u.liked, id)
+}
+
+func (u *User) addNotLiked(id int) {
+   u.notLiked= append(u.notLiked, id)
+}
+
+// Function to read the ratings CSV file and process each row.
+// The output is a map in which user ID is used as key
+func readRatingsCSV(fileName string) (map[int]*User, error) {
+	// Open the CSV file.
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a CSV reader.
+	reader := csv.NewReader(file)
+
+	// Read first line and skip
+	_, err = reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+    // creates the map
+    users:= make(map[int]*User, 1000)
+
+	// Read all records from the CSV.
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	
+	// Iterate over each record and convert the strings into integers or float.
+	for _, record := range records {
+		if len(record) != 4 {
+			return nil, fmt.Errorf("each line must contain exactly 4 integers, but found %d", len(record))
+		}
+
+		// Parse user ID integer
+		uID, err := strconv.Atoi(record[0])
+		if err != nil {
+			return nil, fmt.Errorf("error converting '%s' to userID integer: %v", record[0], err)
+		}
+		
+		// Parse movie ID integer
+		mID, err := strconv.Atoi(record[1])
+		if err != nil {
+			return nil, fmt.Errorf("error converting '%s' to movieID integer: %v", record[1], err)
+		}
+		
+		// Parse rating float
+		r, err := strconv.ParseFloat(record[2],64)
+		if err != nil {
+		    
+			return nil, fmt.Errorf("error converting '%s' to rating: %v", record[2], err)
+		}
+
+		// checks if it is a new user
+		u,ok := users[uID] 
+		if !ok {
+		
+		    u= &User{uID,nil,nil}
+			users[uID]= u
+		}
+
+        // ad movie in user list
+		if r>=iLiked {
+		
+			   u.addLiked(mID)
+			
+		} else {
+		
+			   u.addNotLiked(mID)
+		}
+	}
+
+	return users, nil
+}
+
+// Function to read the movies CSV file and process each row.
+// The output is a map in which user ID is used as key
+func readMoviesCSV(fileName string) (map[int]string, error) {
+	// Open the CSV file.
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a CSV reader.
+	reader := csv.NewReader(file)
+
+	// Read first line and skip
+	_, err = reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+    // creates the map
+    movies:= make(map[int]string, 1000)
+
+	// Read all records from the CSV.
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	
+	// Iterate over each record and convert the strings into integers or float.
+	for _, record := range records {
+		if len(record) != 3 {
+			return nil, fmt.Errorf("each line must contain exactly 3 entries, but found %d", len(record))
+		}
+
+		
+		// Parse movie ID integer
+		mID, err := strconv.Atoi(record[0])
+		if err != nil {
+			return nil, fmt.Errorf("error converting '%s' to movieID integer: %v", record[1], err)
+		}
+		
+		// record 1 is the title
+		movies[mID]= record[1]
+	}
+
+	return movies, nil
+}
+
+
+// checks if value is in the set 
+func member(value int, set []int) bool {
+
+    for _, v := range set {
+        if value == v {
+		
+		   return true
+		}
+    }
+    	
+	return false
+}
+
+
+
+// generator producing Recommendation instances from movie list
+func generateMovieRec(wg *sync.WaitGroup, stop <-chan bool, userID int, titles map[int]string) <-chan Recommendation {
+
+	outputStream := make(chan Recommendation)
+
+	go func() {
+		defer func() {
+		   wg.Done()
+		  }()
+		defer close(outputStream)
+		// defer fmt.Println("\nFin de generateMovieRec...")
+		for k,v := range titles {
+		   select {
+		      case <-stop:
+		         return
+		      case outputStream <- Recommendation{userID, k, v, 0.0, 0}:
+		   }
+		}
+	}()
+	
+	return outputStream
+}
+
+
+
+func main() {
+	
+	fmt.Println("Number of CPUs:", runtime.NumCPU()) // just curious
+	
+    // user to be considered
+	var currentUser int
+	fmt.Println("Recommendations for which user? ")
+    fmt.Scanf("%d",&currentUser)	
+	
+	// Call the function to read and parse the movies CSV file.
+	titles, err := readMoviesCSV("movies1.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Call the function to read and parse the ratings CSV file.
+	ratings, err := readRatingsCSV("ratings1.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// synchronization
+    stop := make(chan bool)
+    var wg sync.WaitGroup
+    wg.Add(1)
+	
+    start := time.Now(); // chrono
+
+	// the sequence of filters
+    recChannel := generateMovieRec(&wg, stop, currentUser, titles)
+	
+	for rec:= range recChannel {
+       fmt.Println(rec)	// oops, do not print to the console when timing	
+	}
+
+    close(stop) // stop all threads
+    wg.Wait()
+	
+	end := time.Now();
+	
+	fmt.Println(ratings[currentUser])
+	
+    fmt.Printf("\n\nExecution time: %s", end.Sub(start))
+}
